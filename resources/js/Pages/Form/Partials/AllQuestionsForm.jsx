be8621a14b70b8b3e2537c354fd7ead21dcd8b77
Eduer Pallares <eduer.pallares@zalvadora.com>
2023-04-06 15:26:57 -0500

    Quita parrajo

--- resources/js/Pages/Form/Partials/AllQuestionsForm.jsx
+++ resources/js/Pages/Form/Partials/AllQuestionsForm.jsx
@@ -5,8 +5,6 @@ export default function AllQuestionsForm({ questions, className = '' }) {
         <section className={className}>
             <header>
                 <h2 className="text-lg font-medium text-gray-900">Questions</h2>
-
-                <p className="mt-1 text-sm text-gray-600">All questions</p>
             </header>
 
             <Question options={questions} />
